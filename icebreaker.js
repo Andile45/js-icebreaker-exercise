3b76bcb9f2be42045577476c80460f3f779d1f56
Mondli <mondli46401@gmail.com>
2025-08-08 11:56:09 +0200

    Partner two Updates..

--- icebreaker.js
+++ icebreaker.js
@@ -3,4 +3,20 @@ const numSiblings = 2;
 const hasSeenInception = false;
 const dreamVecation  = " My dream vecation spot is in Las Vegas While in the U.S state having a bachelor party for one of my friends partying all night"
 
-console.log(`I am patner 1 , my favorite movie is ${favMovie} , i have ${numSiblings} and its ${hasSeenInception} that i have seen the Inception movie. ${dreamVecation}`)
+console.log(`I am patner 1 , my favorite movie is ${favMovie} , i have ${numSiblings} siblings and its ${hasSeenInception} that i have seen the Inception movie. ${dreamVecation}`)
+
+//===== Partner 2 ========
+
+//=== Scenario 5
+let favFood = ["StreetWize","Inyama","Nkomazi"];
+
+//=== Scenario 6 
+let discountCode = "";
+
+//==== Scenario 7 
+let proflePic = null;
+
+//==== Scenario 8
+let starID = BigInt("1234567890123456789012345678901234567890");
+
+
