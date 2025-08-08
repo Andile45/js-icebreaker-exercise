a970d2281942b7511d7c67aaab09547f8acc6500
Andile Manganye <manganyeandile@gmail.com>
2025-08-08 11:51:12 +0200

    Add Partner 1 variables

---
+++ icebreaker.js
@@ -0,0 +1,6 @@
+const favMovie  = "Peaky Blinders";
+const numSiblings = 2;
+const hasSeenInception = false;
+const dreamVecation  = " My dream vecation spot is in Las Vegas While in the U.S state having a bachelor party for one of my friends partying all night"
+
+console.log(`I am patner 1 , my favorite movie is ${favMovie} , i have ${numSiblings} and its ${hasSeenInception} that i have seen the Inception movie. ${dreamVecation}`)
