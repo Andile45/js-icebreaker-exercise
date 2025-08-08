f929977e1de86ce62c7514cce9dca481af6b8ad7
Mondli <mondli46401@gmail.com>
2025-08-08 13:48:01 +0200

    Last commit, all work done

--- icebreaker.js
+++ icebreaker.js
@@ -16,5 +16,53 @@ let proflePic = null;
 //==== Scenario 8
 let starID = BigInt("1234567890123456789012345678901234567890");
 console.log(`My favourate meals are ${favFood} and I got them with a discount offer of ${discountCode}. The profile url is ${proflePic}
-             and the star Id is ${starID}`);
+             and the star Id is ${starID}           `);
+
+
+//==== Part 3 =========
+
+let questLog = ["Collect 10 Firefly Wings","Defeat the Goblin King","Find the Lost Sword of Oakhaven"];
+console.log(questLog);
+
+//== step2 ===
+let x = questLog.length;
+console.log(`The hero has ${x} quests.`);
+
+//== step3 ====
+console.log(questLog[0]);
+
+//== step4 ====
+questLog.push("Deliver the Royal Scroll");
+console.log(questLog);
+
+//== step5 =====
+let completedQuest = questLog.pop();
+console.log("Removed quest:", completedQuest);
+console.log("Updated questLog:", questLog);
+
+//== step6 =====
+questLog.unshift("Rescue the Blacksmith's Cat");
+console.log("Updated questLog:", questLog);
+
+//== step7 =====
+let removedQuest = questLog.shift();
+console.log("Removed quest:", removedQuest);
+console.log("Updated questLog:", questLog);
+
+//== step8 =====
+questLog.splice(1, 1);
+console.log("Updated questLog:", questLog);
+
+
+//== step9 ======
+questLog.splice(1, 0, "Slay the Dragon of Mount Doom");
+console.log("Updated questLog:", questLog);
+
+//== step10 ======
+let questSummary = questLog.join(" | ");
+console.log("Quest Summary:", questSummary);
+
+
+
+
 
